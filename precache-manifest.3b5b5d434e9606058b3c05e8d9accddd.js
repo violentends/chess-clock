ccdcc305bb71e57858e94b89a4074d515e13f091
Timothy McClure <timothy.r.mcclure@gmail.com>
2020-10-15 21:37:34 -0500

    Updates

---
+++ precache-manifest.3b5b5d434e9606058b3c05e8d9accddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b4d448228db150a7f37c9199bd1494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be1cc52eb9ae4778430f",
+    "url": "/static/css/main.76221818.chunk.css"
+  },
+  {
+    "revision": "7c5fdbb96a4d5de4f4e7",
+    "url": "/static/js/2.7f87d364.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f87d364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be1cc52eb9ae4778430f",
+    "url": "/static/js/main.c8e419a6.chunk.js"
+  },
+  {
+    "revision": "eca9a732a38201e26ec9",
+    "url": "/static/js/runtime-main.34644ce9.js"
+  }
+]);
